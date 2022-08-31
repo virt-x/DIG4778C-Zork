2a590f7b10c42890ad351e8295713ac82c23fcf5
virt-x <xvirt@knights.ucf.edu>
2022-08-31 15:15:39 -0400

    Initial commit for Zork 1.3

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,10 +8,34 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.ToUpper().Trim());
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().ToUpper().Trim());
 
-            Console.WriteLine(command);
+                string outputString;
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing!";
+                        break;
+                    case Commands.LOOK:
+                        outputString = "This is an open field West of a white house, with a boarded front door.\n\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.EAST:
+                    case Commands.WEST:
+                        outputString = $"You moved {command.ToString()}.";
+                        break;
+                    default:
+                        outputString = "Unknown command.";
+                        break;
+                }
+
+                Console.WriteLine(outputString);
+            }
         }
 
         private static Commands ToCommand(string commandString)
