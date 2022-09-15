516c7f9e893f40441fc2f9e70add605c42522060
virt-x <xvirt@outlook.com>
2022-09-14 22:38:03 -0400

    Corrections for Zork 2.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,34 +1,20 @@
 ﻿using System;
-
 namespace Zork
 {
     class Program
     {
-        private static readonly Room[,] _rooms = {
-            { new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
-            { new Room("Forest"), new Room("West of House"), new Room("Behind House") },
-            { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
-        };
-        private static (byte row, byte column) _location = (1,1);
 
-        private static Room CurrentRoom
-        {
-            get
-            {
-                return _rooms[_location.row, _location.column];
-            }
-        }
+        private static readonly string[] _rooms = { "Forest", "West of House", "Behind House", "Clearing", "Canyon View" };
+        private static byte _currentRoom = 1;
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
 
-            InitializeRoomDescriptions();
-
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
             {
-                Console.Write($"{CurrentRoom}\n> ");
+                Console.Write($"{ _rooms[_currentRoom]}\n> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
                 string outputString;
@@ -38,7 +24,7 @@ namespace Zork
                         outputString = "Thank you for playing!";
                         break;
                     case Commands.LOOK:
-                        outputString = CurrentRoom.Description;
+                        outputString = "This is an open field West of a white house, with a boarded front door.\n\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
                         break;
                     case Commands.NORTH:
                     case Commands.SOUTH:
@@ -50,11 +36,9 @@ namespace Zork
                         outputString = "Unknown command.";
                         break;
                 }
-
                 Console.WriteLine(outputString);
             }
         }
-
         private static Commands ToCommand(string commandString)
         {
             return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
@@ -64,40 +48,22 @@ namespace Zork
         {
             switch (command)
             {
-                case Commands.NORTH when _location.row < _rooms.GetLength(0) - 1:
-                    _location.row++;
+                case Commands.NORTH:
+                case Commands.SOUTH:
+                    break;
+
+                case Commands.EAST when _currentRoom < _rooms.Length - 1:
+                    _currentRoom++;
                     return true;
 
-                case Commands.SOUTH when _location.row > 0:
-                    _location.row--;
-                    return true;
-
-                case Commands.EAST when _location.column < _rooms.GetLength(1) - 1:
-                    _location.column++;
-                    return true;
-
-                case Commands.WEST when _location.column > 0:
-                    _location.column--;
+                case Commands.WEST when _currentRoom > 0:
+                    _currentRoom--;
                     return true;
 
                 default:
                     return false;
             }
-        }
-
-        private static void InitializeRoomDescriptions()
-        {
-            _rooms[0, 0].Description = "You are on a rock-strewn trail.";
-            _rooms[0, 1].Description = "Yo are facing the south side of a white house. There is no door here, and all the windows are barred.";
-            _rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall.";
-
-            _rooms[1, 0].Description = "This is a forest, with trees in all directinos around you.";
-            _rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door.";
-            _rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
-
-            _rooms[2, 0].Description = "This is a dimly lit forest, with large trees alll around. To the east, there appears to be sunlight.";
-            _rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
-            _rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
+            return false;
         }
     }
 }
